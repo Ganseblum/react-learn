e8a35486470b98a901658c09ea09bf89b1efbe33
Ganseblum <977252487@qq.com>
2022-07-22 07:06:56 +0800

    打卡学习

--- src/component/maizuoComponent/search.js
+++ src/component/maizuoComponent/search.js
@@ -2,30 +2,37 @@ import axios from 'axios'
 import React, { Component } from 'react'
 
 export default class search extends Component {
-  constructor() {
-    super()
-    axios({
-      url: 'https://m.maizuo.com/gateway?cityId=310100&ticketFlag=1&k=7099440',
-      headers: {
-        'X-Client-Info': '{ "a": "3000", "ch": "1002", "v": "5.2.0", "e": "16584148861332367574695937", "bc": "110100" }',
-        'X-Host': 'mall.cfg.cinema.banners'
-      },
-      method: 'get'
-    }).then(res => {
-      console.log(res)
-    })
-  }
   state = {
     list: []
   }
+  constructor() {
+    super()
+    axios({
+      url: 'http://jsonplaceholder.typicode.com/posts',
+      // headers: {
+      //   'X-Client-Info': '{ "a": "3000", "ch": "1002", "v": "5.2.0", "e": "16584148861332367574695937", "bc": "110100" }',
+      //   'X-Host': 'mall.cfg.cinema.banners'
+      // },
+      method: 'get'
+    }).then(res => {
+      this.setState({
+        list: res.data
+      })
+    })
+  }
+
+
   getList (e) {
-    console.log(e)
+    console.log(e.target.value)
   }
   render () {
     return (
       <div>
-        <div>1</div>
-      </div>
+        <input type="text" onInput={(e) => { this.getList(e) }}></input>
+        <div>
+          {this.state.list.map(item => <div key={item.id}>{item.title}</div>)}
+        </div>
+      </div >
     )
   }
 }
